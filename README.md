abf03d24cdf912af2dd15f9b33c4cc6d9947843f
David Cumps <david@cumps.be>
2015-10-27 17:29:44 +0100

    Start rewrite to F#

---
+++ README.md
@@ -0,0 +1,9 @@
+# aws-s3-cache-cleanup [![NuGet Status](http://img.shields.io/nuget/v/Exira.AwsS3Cache.Cleanup.svg?style=flat)](https://www.nuget.org/packages/Exira.AwsS3Cache.Cleanup/)
+
+## Exira.AwsS3Cache.Cleanup
+
+Exira.AwsS3Cache.Cleanup is a task which cleans up expired cached objects from AWS S3
+
+### Cloning
+
+```git clone git@github.com:exira/aws-s3-cache-cleanup.git -c core.autocrlf=input```
---
+++ docs/content/index.fsx
@@ -0,0 +1,35 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+(**
+aws-s3-cache-cleanup [![NuGet Status](http://img.shields.io/nuget/v/Exira.AwsS3Cache.Cleanup.svg?style=flat)](https://www.nuget.org/packages/Exira.AwsS3Cache.Cleanup/)
+======================
+
+<div class="row">
+  <div class="span1"></div>
+  <div class="span6">
+    <div class="well well-small" id="nuget">
+      The aws-s3-cache-cleanup library can be <a href="https://nuget.org/packages/Exira.AwsS3Cache.Cleanup">installed from NuGet</a>:
+      <pre>PM> Install-Package Exira.AwsS3Cache.Cleanup</pre>
+    </div>
+  </div>
+  <div class="span1"></div>
+</div>
+
+Contributing and copyright
+--------------------------
+
+The project is hosted on [GitHub][gh] where you can [report issues][issues], fork
+the project and submit pull requests. You might also want to read the
+[library design notes][readme] to understand how it works.
+
+For more information see the [License file][license] in the GitHub repository.
+
+  [content]: https://github.com/exira/aws-s3-cache-cleanup/tree/master/docs/content
+  [gh]: https://github.com/exira/aws-s3-cache-cleanup
+  [issues]: https://github.com/exira/aws-s3-cache-cleanup/issues
+  [readme]: https://github.com/exira/aws-s3-cache-cleanup/blob/master/README.md
+  [license]: https://github.com/exira/aws-s3-cache-cleanup/blob/master/LICENSE.txt
+*)
