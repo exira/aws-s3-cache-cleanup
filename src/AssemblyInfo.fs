5b9d940d743e48c185590aa3794b81d2ddcb2b43
Team City <info@exira.com>
2015-12-28 15:04:40 +0000

    Bump version to 0.1.4

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("aws-s3-cache-cleanup")>]
 [<assembly: AssemblyProductAttribute("Exira.AwsS3Cache.Cleanup")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.AwsS3Cache.Cleanup is a task which cleans up expired cached objects from AWS S3")>]
-[<assembly: AssemblyVersionAttribute("0.1.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.3")>]
-[<assembly: AssemblyMetadataAttribute("githash","fa07032a6f8dcbd99826733a78f0149595845c34")>]
+[<assembly: AssemblyVersionAttribute("0.1.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.4")>]
+[<assembly: AssemblyMetadataAttribute("githash","834e0f511c56dc5f4000fc05648baa0b85acde22")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.3"
+    let [<Literal>] Version = "0.1.4"
